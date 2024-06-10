2f08095e6e35c204c2a15e33f4139733447cb70b
simarjeet <simarjeet.sm7@gmail.com>
2024-06-11 00:35:43 +0530

    added optimisation to the bubble sort by adding swapping flag

--- DSA/sorting_algo/bubble_sort.cpp
+++ DSA/sorting_algo/bubble_sort.cpp
@@ -13,11 +13,17 @@ void bubble_sort(vector<int> &v){
     int n = v.size();
 
     for (int i = n - 1;i>=0;i--){
+        bool swapped = false;
         for (int j = 0 ;j<=i-1;j++){
             if (v[j]>v[j+1]){
                 swap (v[j],v[j+1]);
+                swapped = true;
             }
+            
         }
+        if (!swapped){
+                break ;
+            }
     }
 }
 
